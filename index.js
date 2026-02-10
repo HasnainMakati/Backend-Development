785434c57fc3bb99d965159edbe8bb0465d5f21a
hasnain <mkthassu72@gmail.com>
2026-02-10 18:46:36 +0530

    .

--- index.js
+++ index.js
@@ -8,6 +8,43 @@ const app = express()
 // listen  server port ()
 const port = 4000
 
+const githubData = {
+    "login": "HasnainMakati",
+    "id": 259584014,
+    "node_id": "U_kgDOD3jwDg",
+    "avatar_url": "https://avatars.githubusercontent.com/u/259584014?v=4",
+    "gravatar_id": "",
+    "url": "https://api.github.com/users/HasnainMakati",
+    "html_url": "https://github.com/HasnainMakati",
+    "followers_url": "https://api.github.com/users/HasnainMakati/followers",
+    "following_url": "https://api.github.com/users/HasnainMakati/following{/other_user}",
+    "gists_url": "https://api.github.com/users/HasnainMakati/gists{/gist_id}",
+    "starred_url": "https://api.github.com/users/HasnainMakati/starred{/owner}{/repo}",
+    "subscriptions_url": "https://api.github.com/users/HasnainMakati/subscriptions",
+    "organizations_url": "https://api.github.com/users/HasnainMakati/orgs",
+    "repos_url": "https://api.github.com/users/HasnainMakati/repos",
+    "events_url": "https://api.github.com/users/HasnainMakati/events{/privacy}",
+    "received_events_url": "https://api.github.com/users/HasnainMakati/received_events",
+    "type": "User",
+    "user_view_type": "public",
+    "site_admin": false,
+    "name": "Hasnain Makati",
+    "company": null,
+    "blog": "",
+    "location": null,
+    "email": null,
+    "hireable": null,
+    "bio": null,
+    "twitter_username": null,
+    "public_repos": 2,
+    "public_gists": 0,
+    "followers": 1,
+    "following": 0,
+    "created_at": "2026-02-05T12:59:28Z",
+    "updated_at": "2026-02-08T12:51:15Z"
+}
+
+
 app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
 })
@@ -22,6 +59,10 @@ app.get('/login', (req, res) => {
 
 app.get('/appsile', (req, res) => {
     res.send("<h1>appsile.com</h1>")
+})
+
+app.get('/github', (req, res) => {
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
